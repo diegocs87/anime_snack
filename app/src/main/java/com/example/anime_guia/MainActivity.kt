325268874614765de52009da5a2951dfca7ecac6
diegocs87 <dlcardenas83@gmail.com>
2022-08-11 18:52:44 -0500

    View Model, LiveData and Fragments dependencies added and building.

--- app/src/main/java/com/example/anime_guia/MainActivity.kt
+++ app/src/main/java/com/example/anime_guia/MainActivity.kt
@@ -2,10 +2,11 @@ package com.example.anime_guia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.database.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
 }
