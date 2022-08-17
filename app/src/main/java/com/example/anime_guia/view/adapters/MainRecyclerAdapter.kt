7218ff01dac6ddd996947dd0ee7f9898e485b408
diegocs87 <dlcardenas83@gmail.com>
2022-08-16 22:25:36 -0500

    Recycler view item layout created

--- app/src/main/java/com/example/anime_guia/view/adapters/MainRecyclerAdapter.kt
+++ app/src/main/java/com/example/anime_guia/view/adapters/MainRecyclerAdapter.kt
@@ -1,24 +1,26 @@
 package com.example.anime_guia.view.adapters
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.Adapter
+import androidx.recyclerview.widget.RecyclerView.inflate
+import com.example.anime_guia.R
 import com.example.anime_guia.model.Anime
 
-class MainRecyclerAdapter(var animes : List<Anime>) : Adapter<MainRecyclerAdapter.viewHolder>() {
+class MainRecyclerAdapter(private val animes : List<Anime>) : Adapter<MainRecyclerAdapter.viewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): viewHolder {
-        TODO("Not yet implemented")
+        return viewHolder(LayoutInflater.from(parent.context).
+        inflate(R.layout.anime_card_view_item, parent, false))
     }
 
     override fun onBindViewHolder(holder: viewHolder, position: Int) {
         TODO("Not yet implemented")
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount(): Int = animes.size
 
     class viewHolder(view : View): RecyclerView.ViewHolder(view) {
 
