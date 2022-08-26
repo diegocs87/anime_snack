4db4a3a47c6eb7593139a5c025c988b868d5c784
diegocs87 <dlcardenas83@gmail.com>
2022-08-26 12:18:53 -0500

    Loading Main Activity Image View with Glider

--- app/src/main/java/com/example/anime_guia/view/ShowAnimeDetailActivity.kt
+++ app/src/main/java/com/example/anime_guia/view/ShowAnimeDetailActivity.kt
@@ -2,6 +2,7 @@ package com.example.anime_guia.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import com.example.anime_guia.databinding.AnimeDetailLayoutBinding
 
 class ShowAnimeDetailActivity : AppCompatActivity() {
@@ -9,5 +10,10 @@ class ShowAnimeDetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val bindingDetailView = AnimeDetailLayoutBinding.inflate(layoutInflater)
         setContentView(bindingDetailView.root)
+        Glide
+            .with(bindingDetailView.root.context)
+            .load("https://cdn.myanimelist.net/images/anime/1223/96541.jpg")
+            .centerCrop()
+            .into(bindingDetailView.AnimeCover)
     }
 }
