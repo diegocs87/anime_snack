09ae8fe2370f6acff515db0138c7b32e9359af17
diegocs87 <dlcardenas83@gmail.com>
2022-08-26 14:59:35 -0500

    Youtube Player to play Anime Trailer added on AnimeDetail view

--- app/src/main/java/com/example/anime_guia/view/ShowAnimeDetailActivity.kt
+++ app/src/main/java/com/example/anime_guia/view/ShowAnimeDetailActivity.kt
@@ -1,11 +1,16 @@
 package com.example.anime_guia.view
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.bumptech.glide.Glide
 import com.example.anime_guia.databinding.AnimeDetailLayoutBinding
+import com.google.android.youtube.player.YouTubeBaseActivity
+import com.google.android.youtube.player.YouTubeInitializationResult
+import com.google.android.youtube.player.YouTubePlayer
+import com.google.android.youtube.player.YouTubePlayerView
 
-class ShowAnimeDetailActivity : AppCompatActivity() {
+class ShowAnimeDetailActivity : YouTubeBaseActivity(), YouTubePlayer.OnInitializedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val bindingDetailView = AnimeDetailLayoutBinding.inflate(layoutInflater)
@@ -15,5 +20,25 @@ class ShowAnimeDetailActivity : AppCompatActivity() {
             .load("https://cdn.myanimelist.net/images/anime/1223/96541.jpg")
             .centerCrop()
             .into(bindingDetailView.AnimeCover)
+        val  view = bindingDetailView.youtubePlayer
+        view.initialize("AIzaSyBYYK35yBg_7EWi-DhFxDOV2ls1s-Vi_Xw",this)
+    }
+
+    override fun onInitializationSuccess(
+        p0: YouTubePlayer.Provider?,
+        p1: YouTubePlayer?,
+        p2: Boolean
+    ) {
+        if (!p2) {
+            println("on p2")
+            p1!!.cueVideo("--IcmZkvL0Q");
+        }
+    }
+
+    override fun onInitializationFailure(
+        p0: YouTubePlayer.Provider?,
+        p1: YouTubeInitializationResult?
+    ) {
+        TODO("Not yet implemented")
     }
 }
