32a28242cd05992d713b46051f52233618d74aa9
diegocs87 <dlcardenas83@gmail.com>
2022-09-01 21:22:31 -0500

    serialized names tags added on Anime data class

--- app/src/main/java/com/example/anime_guia/model/Anime.kt
+++ app/src/main/java/com/example/anime_guia/model/Anime.kt
@@ -1,14 +1,26 @@
 package com.example.anime_guia.model
 
+import com.google.gson.annotations.SerializedName
+
 data class Anime (
-    val tittle: String,
+    @SerializedName("tittle")
+    val  tittle: String,
+    @SerializedName("episodes")
     val episodes: String,
+    @SerializedName("studios")
     val studios: String,
-    val image: String,
+    @SerializedName("image")
+    val cover: String,
+    @SerializedName("rank")
     val rank:String,
+    @SerializedName("score")
     val score: String,
+    @SerializedName("status")
     val status: String,
+    @SerializedName("synopsis")
     val synopsis: String,
+    @SerializedName("trailer")
     val trailer: String,
+    @SerializedName("genres")
     val genres: String
 )
